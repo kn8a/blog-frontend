935c07bf281407259400253649989453df2b65eb
kn8a <88045655+kn8a@users.noreply.github.com>
2022-07-22 11:25:21 +0700

    fetch comments for post

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -7,19 +7,26 @@ import Spinner from '../components/Spinner';
 function Post(props) {
 
   const params = useParams()
-  const baseURL = `https://kn8a-blog-api.herokuapp.com/api/posts/${params.postId}`
+  const postURL = `https://kn8a-blog-api.herokuapp.com/api/posts/${params.postId}`
+  const commentsURL = `https://kn8a-blog-api.herokuapp.com/api/posts/${params.postId}/comments`
 
   const [post, setPost] = useState(null);
+  const [comments, setComments] = useState(null)
+
 
   useEffect(() => {
-    axios.get(`${baseURL}`).then((response) => {
+    axios.get(`${postURL}`).then((response) => {
       setPost(response.data);
-
     });
+    axios.get(`${commentsURL}`).then((response) => {
+        setComments(response.data);
+      });
   }, []);
 
-  if (!post) return <Spinner/>
-  console.log(post)
+
+  if (!post || !comments) return <Spinner/>
+  console.log(post, comments)
+
 
   return (
     <div>Post</div>
@@ -27,3 +34,5 @@ function Post(props) {
 }
 
 export default Post
+
+
